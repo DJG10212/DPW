04de18b7e4d2c558a7e91cafefde01ea3081a0f3
DJG10212 <dgillette@fullsail.edu>
2015-10-10 19:13:30 -0700

    working on getting the page.py working

--- simple-form/simple-form/page.py
+++ simple-form/simple-form/page.py
@@ -1,32 +1,44 @@
-page_head ='''<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Pizza Order Form</title>
-    </head>
-    <body>'''
+'''
+Dakota Gillette
+10/8/15
+DWP
+Simple Form
+'''
 
-        page_body = '''<form method="GET">
-            <label>Name: </label><input type="text" name="user" />
-            <br><br>
-            <label>Phone Number </label><input type="text" name="phone"/>
-            <br><br>
-            <label>Your Address </label><input type="text" name="address"/>
-            <br><br>
-            <label>Pizza Type: </label><select name="pizzaType">
-            <option value="pepperoni">Pepperoni</option>
-            <option value="cheese">Cheese</option>
-            <option value="hawaiian">Hawaiian</option>
-            <option value="supreme">Supreme</option>
-            </select>
-            <br><br>
-            <label>Select One: </label><input type="radio" name="orderType" value="delivery" checked>Delivery
-            <input type="radio" name="orderType" value="carryout">Carryout
-            <br><br>
-            <textarea name="request" rows="10" cols="30">Leave any special requests in this field</textarea>
-            <br><br>
-            <input type="submit" value="Submit" />'''
 
-        page_close = '''
-        </form>
-    </body>
-</html>'''
+class Page(): #page class
+    def __init__(self, page_main): # set instances for class
+        self.css = "css/style.css" # grabbing the stylesheet for the page
+        self.head = '''<!DOCTYPE HTML>  <!-- initializing the HTML -->
+        <html>
+            <head>
+                <title>Pizza Order Form</title>
+            </head>
+            <body>'''
+
+        self.body = '''<form method="GET">
+                    <label>Name: </label><input type="text" name="user" />
+                    <br><br>
+                    <label>Phone Number </label><input type="text" name="phone"/>
+                    <br><br>
+                    <label>Your Address </label><input type="text" name="address"/>
+                    <br><br>
+                    <label>Pizza Type: </label><select name="pizzaType">
+                    <option value="pepperoni">Pepperoni</option>
+                    <option value="cheese">Cheese</option>
+                    <option value="hawaiian">Hawaiian</option>
+                    <option value="supreme">Supreme</option>
+                    </select>
+                    <br><br>
+                    <label>Select One: </label><input type="radio" name="orderType" value="delivery" checked>Delivery
+                    <input type="radio" name="orderType" value="carryout">Carryout
+                    <br><br>
+                    <textarea name="request" rows="10" cols="30">Leave any special requests in this field</textarea>
+                    <br><br>
+                    <input type="submit" value="Submit" />'''
+
+        self.close = '''
+                </form>
+            </body>
+        </html>'''
+        

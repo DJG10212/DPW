16ccb80bc31f475450247876d9a32e64a250ea10
DJG10212 <dgillette@fullsail.edu>
2015-10-10 17:57:23 -0700

    setting up form

---
+++ Simple Form/Simple Form/main.py
@@ -0,0 +1,42 @@
+'''
+Dakota Gillette
+10/10/15
+DWP
+Simple Form
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+    page_head ='''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>'''
+
+        page_body = '''<form method="GET">
+            <label>Name: </label><input type="text" name="user" />
+            <label>Email: </label><input type="text" name="email"/>
+            <input type="submit" value="Submit" />'''
+
+        page_close = '''
+        </form>
+    </body>
+</html>'''
+
+
+        if self.request.GET:
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            self.response.write(page_head + user + ' ' + email + page_body + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close) #PRINT
+        # code goes here
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)

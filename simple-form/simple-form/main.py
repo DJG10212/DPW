0e769dc352139a0703b2a6ae8c33a08d957ddac6
DJG10212 <dgillette@fullsail.edu>
2015-10-10 19:03:25 -0700

    moving form to page.py, and taking it off of main.py

--- simple-form/simple-form/main.py
+++ simple-form/simple-form/main.py
@@ -2,6 +2,7 @@
 Dakota Gillette
 10/8/15
 DWP
+Simple Form
 '''
 
 import webapp2  # use the webapp2 library
@@ -9,29 +10,7 @@ import webapp2  # use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler):  # declaring a class
     def get(self): # function that starts everything. Catalyst
-        page_head ='''<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Pizza Order Form</title>
-    </head>
-    <body>'''
 
-        page_body = '''<form method="GET">
-            <label>Name: </label><input type="text" name="user" />
-            <label>Phone Number </label><input type="text" name="phone"/>
-            <label>Your Address </label><input type="text" name="address"/>
-            <input type="submit" value="Submit" />
-            <label>Pizza Type: </label><select name="pizzaType">
-            <option value="pepperoni">Pepperoni</option>
-            <option value="cheese">Cheese</option>
-            <option value="hawaiian">Hawaiian</option>
-            <option value="supreme">Supreme</option>
-            </select>'''
-
-        page_close = '''
-        </form>
-    </body>
-</html>'''
 
         if self.request.GET:
             user = self.request.GET['user']

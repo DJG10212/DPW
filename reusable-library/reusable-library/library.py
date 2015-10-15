4bd839cd0e4abfa5d16ea3db36c82df6a0a15377
DJG10212 <dgillette@fullsail.edu>
2015-10-15 14:34:02 -0700

    working on getter/setter

--- reusable-library/reusable-library/library.py
+++ reusable-library/reusable-library/library.py
@@ -1,3 +1,17 @@
 class Paycheck(object):
     def __init__(self):
-        
+        # setting some default values
+        self.__twice = False
+        self.__wage = 0
+        self.__hours = 0
+        self.__holiday = 0
+
+    # Twice a month getter/setter
+
+    @property
+    def twice(self):
+        return self.__twice
+
+    @twice.setter
+    def twice(self, y):
+        self.__twice = y

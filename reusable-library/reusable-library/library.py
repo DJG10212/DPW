fa6fca573deec4b5d59637156febb89b2c0b8555
DJG10212 <dgillette@fullsail.edu>
2015-10-15 15:49:58 -0700

    testing calc

--- reusable-library/reusable-library/library.py
+++ reusable-library/reusable-library/library.py
@@ -52,3 +52,22 @@ class Calculate(object):
         # getting the total
     def total_up(self, c):
         self.__info.append(c)
+        __holiday_hours = int(c.holiday) * 1.5
+        __holiday_pay = __holiday_hours * int(c.wage)
+        total = int(c.wage) * int(c.hours) + __holiday_pay
+        print c.twice
+        # if they get paid twice a month, divide by 2
+        if c.twice == "True":
+            print "Pay check twice a month"
+            total = total / 2
+        else:
+            print "Pay check once a month"
+        return total
+
+        # this will sum up everything once we get the total
+    def summary_text(self, c):
+            total = self.total_up(c)
+            print total
+            print Paycheck.wage
+            # returns the info
+            return "<h3>Paycheck: $" + str(total) +"<h3>"

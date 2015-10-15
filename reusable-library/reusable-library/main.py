88d449a700d48c87e61a62db43cd5acf5e5167fb
DJG10212 <dgillette@fullsail.edu>
2015-10-15 11:08:00 -0700

    ran through lessons, now starting my own. Thinking I'll do something for paychecks

--- reusable-library/reusable-library/main.py
+++ reusable-library/reusable-library/main.py
@@ -1,27 +1,13 @@
 
 
 import webapp2
-from library import MovieData, FavoriteMovies
-from pages import Page
+
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        p = Page()
-        lib = FavoriteMovies()
 
-        md1 = MovieData()
-        md1.title = "The Princess Bride"
-        md1.year = 1989 # actually calling a function
-        md1.director = "Rob Reiner"
-        lib.addMovie(md1)
 
-        md2 = MovieData()
-        md2.title = "Dune"
-        md2.year = 1986 # actually calling a function
-        md2.director = "David Lynch"
-
-        self.response.write(p.print_out())
 
 
 

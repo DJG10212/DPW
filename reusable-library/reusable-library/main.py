c6f40cf8a64268989013bf7f9e04b5e66dd99dbe
DJG10212 <dgillette@fullsail.edu>
2015-10-15 13:46:59 -0700

    working on main.py

--- reusable-library/reusable-library/main.py
+++ reusable-library/reusable-library/main.py
@@ -1,13 +1,17 @@
 
 
 import webapp2
-
+from pages import Page, ResultsPage
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
-
+    if self.request.GET:
+        pc1.wage = self.request.GET['wage']
+        pc1.hours = self.request.GET['hours']
+        pc1.holiday = self.request.GET['holiday']
+        pc1.twice = self.request.GET['twice']
 
 
 

7c6ec95fb972f92e274a010467d6c871a85d1e0f
DJG10212 <dgillette@fullsail.edu>
2015-10-21 22:58:12 -0700

    updating pages changes to index and main

--- dynamic-site/dynamic-site/main.py
+++ dynamic-site/dynamic-site/main.py
@@ -6,7 +6,7 @@ from pages import *  # grabs all classes from the pages.py page
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         landing_page = Page()  # creates an instance of Page
-        self.response.write(landing_page.main_view())  # sends view to user
+        self.response.write(landing_page.main_view("index"))  # sends view to user
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)

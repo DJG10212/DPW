4a88a98ca6b22f4e5225fea960b9a2742f895c4d
DJG10212 <dgillette@fullsail.edu>
2015-10-21 23:19:32 -0700

    added content page/pages changes

--- dynamic-site/dynamic-site/pages.py
+++ dynamic-site/dynamic-site/pages.py
@@ -1,10 +1,9 @@
 class Page(object):
     def __init__(self):
-        pass
-
-    def main_view(self, desired_view_name):
         self._title = "Welcome"  # page title
-        self._view_name = desired_view_name
+        self._view_name = "testing"
+
+    def main_view(self):
         self._view = open(str("Pages/{view_name}.html").format(view_name=self._view_name))
         return self._view.read().format(**locals())  # reads then inserts variables and returns to user
 
@@ -21,4 +20,5 @@ class Page(object):
 
 class ContentPage(Page):
     def __init__(self):
-        pass
+        self._title = "Content Page"
+        self._view_name = "Content"

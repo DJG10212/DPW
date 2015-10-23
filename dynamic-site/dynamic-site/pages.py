a4f4ff5e615066704d0e7587ddf6f97f4469e79c
DJG10212 <dgillette@fullsail.edu>
2015-10-23 00:12:20 -0700

    updated pages.py

--- dynamic-site/dynamic-site/pages.py
+++ dynamic-site/dynamic-site/pages.py
@@ -1,34 +1,28 @@
 class Page(object):
     def __init__(self):
         self._title = None  # page title
-        self._view_name = "Welcome"
 
     def main_view(self):
-        self._view = open(str("Pages/{view_name}.html").format(view_name=self._view_name.lower()))
-        return self._view.read().format(**locals())  # reads then inserts variables and returns to user
+        return open("Pages/Credit.html").read()  # reads page and returns
 
     # getter
     @property
     def title(self):
         return self._title
 
-    @property
-    def view_name(self):
-        return self._view_name
 
     # setter
     @title.setter
     def title(selfself, new_title):
         self._title = new_title
 
-    @view_name.setter
-    def view_name(self, new_view_name):
-        self._view_name = new_view_name
-
 
 class MoviePage(Page):
     def __init__(self):
         self._movie = None  # movie objects
+
+    def render_view(self):
+        return open("Pages/Content.html").read().format(**locals())
 
     # getter
     @property
